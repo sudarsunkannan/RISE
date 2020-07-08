e2530d4bdc2141e8cdca465f4496c281bbf78701
kannan11 <kannan11@node-0.node2.rise-pg0.clemson.cloudlab.us>
2020-07-08 00:27:49 -0400

    changes

---
+++ cassandra/code/scripts/install_libs.sh
@@ -0,0 +1,33 @@
+sudo apt-get install -y git
+#git commit --amend --reset-author
+
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y python3-software-properties
+sudo apt-get install -y python-software-properties
+sudo apt-get install -y unzip
+sudo apt-get install -y python-setuptools python-dev build-essential
+sudo easy_install pip
+sudo apt-get install -y numactl
+sudo apt-get install -y libsqlite3-dev
+sudo apt-get install -y libnuma-dev
+sudo apt-get install -y libkrb5-dev
+sudo apt-get install -y libsasl2-dev
+sudo apt-get install -y cmake
+sudo apt-get install -y build-essential
+sudo apt-get install -y maven
+sudo apt-get install -y fio
+sudo apt-get install -y libbfio-dev
+sudo apt-get install -y libboost-dev
+sudo apt-get install -y libboost-thread-dev
+sudo apt-get install -y libboost-system-dev
+sudo apt-get install -y libboost-program-options-dev
+sudo apt-get install -y libconfig-dev
+sudo apt-get install -y uthash-dev
+sudo apt-get install -y libmpich2-dev
+sudo apt-get install -y libglib2.0-dev
+sudo apt-get install -y cscope
+sudo apt-get install -y msr-tools
+sudo apt-get install -y msrtool
+sudo apt-get install -y textinfo
+sudo apt-get install -y dpkg
+sudo pip install psutil
